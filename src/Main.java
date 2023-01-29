a975ad20282ffd3cc8d14797268da3c30a79507b
giova <giovannitopor@gmail.com>
2023-01-30 00:06:19 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+        public static void main(String[] args) {
+            String name = "Giovanni";
+            String surname ="Topor";
+
+            System.out.println(name);
+            System.out.println(surname);
+        }
+    }
+
